cdff437ee422ac4e6f8df545380c041a91cc96c7
Aleksandra Müller <aleksandra.mueller@wzl.rwth-aachen.de>
2021-04-23 17:33:46 +0200

    initial version of c5-task, to be changed

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '1615975973:AAE1lqa_nNF4CTKbU-ZFSBZN5jbkFhPqWpI'
+keys={
+    'euro' : 'EUR',
+    'dollar' : 'USD',
+    'ruble' : 'RUB',
+}
---
+++ utils.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConvertionException(f"You don't need to convert {quote} in {quote}, don't you?")
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"It was not possible to process currency {quote}")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f"It was not possible to process currency {base}")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'{amount} could not be processed')
+
+        r_base = requests.get(f'http://api.exchangeratesapi.io/v1/latest?access_key=27c1bfcad0c47501c40be7e870cdd857&symbols={base_ticker}&format=1')
+        r_quote = requests.get(f'http://api.exchangeratesapi.io/v1/latest?access_key=27c1bfcad0c47501c40be7e870cdd857&symbols={quote_ticker}&format=1')
+
+        total_base = json.loads(r_base.content)['rates'][keys[base]]
+        total_quote = json.loads(r_quote.content)['rates'][keys[quote]]
+        return round((total_base/total_quote)*amount,2)
+
+
